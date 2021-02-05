021f21b539bbfe371bc4a411ba6a7b5cc2ea2efd
nely de faria <nelly.nf54@gmail.com>
2021-02-05 17:40:30 -0300

    Adiciona arquivo test-session.php

---
+++ php-advanced/sessoes/test-session.php
@@ -0,0 +1,9 @@
+<?php
+require_once 'Session.php';
+
+$session = new Session('TREINAWEB');
+
+$session->set('nome', 'Leonardo');
+$session->set('email', 'leo@teste.com');
+
+$session->register('20 sec');
