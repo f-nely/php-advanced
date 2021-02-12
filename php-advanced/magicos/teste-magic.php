741d4abca3fb439bd2caa0ae3c49f983f21da766
nely de faria <nelly.nf54@gmail.com>
2021-02-12 20:07:51 -0300

    Comenta bloco de código e imprime list

--- php-advanced/magicos/teste-magic.php
+++ php-advanced/magicos/teste-magic.php
@@ -6,8 +6,10 @@ $list = new MagicList();
 $list->name = 'Leonardo';
 $list->email = 'leonardo@test.com';
 
-echo $list->name;
+/*echo $list->name;
 echo '<br>';
-echo $list->email;
+echo $list->email;*/
+
+echo $list;
 
 var_dump($list);
