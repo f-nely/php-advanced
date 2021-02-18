a1ebe3d2000c9cc1cd2b37de18c67d47a6aa85ea
nely de faria <nelly.nf54@gmail.com>
2021-02-18 12:46:09 -0300

    Adiciona classe SlackNotification

---
+++ php-advanced/inversao-de-depedencia/SlackNotification.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace Treinaweb;
+
+
+class SlackNotification implements NotificationInterface
+{
+
+    public function send()
+    {
+        // TODO: Implement send() method.
+    }
+}
