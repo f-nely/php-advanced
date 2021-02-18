9fefc2b3a064d249490554f36c0b2e0ad34a3092
nely de faria <nelly.nf54@gmail.com>
2021-02-18 12:44:29 -0300

    Adiciona classe Notification

---
+++ php-advanced/inversao-de-depedencia/Notification.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace Treinaweb;
+
+
+class Notification implements NotificationInterface
+{
+
+    public function send()
+    {
+        // TODO: Implement send() method.
+    }
+}
