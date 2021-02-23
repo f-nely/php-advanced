640c77d85f2491b4ee0631ed48ef7b133e28df15
nely de faria <nelly.nf54@gmail.com>
2021-02-23 18:57:12 -0300

    Refatorando código

--- php-advanced/symfony/index.php
+++ php-advanced/symfony/index.php
@@ -1,12 +1,31 @@
 <?php
 
-require_once 'vendor/autoload.php';
+require 'vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Treinaweb\Core;
 
 $app = new Core();
-$request = Request::createFromGlobals();
 
+// Rotas.
+$app->route('/', function(){
+    return new Response('Index');
+});
+
+$app->route('/contato', function(){
+    return new Response('Página de contato.');
+});
+
+$app->route('/sobre', function(){
+    return new Response('Sobre o projeto.');
+});
+
+$app->route('/teste', function (){
+   return new Response('Apenas um teste. :-D');
+});
+
+// Lida com a requisição
+$request = Request::createFromGlobals();
 $response = $app->handle($request);
 $response->send();
